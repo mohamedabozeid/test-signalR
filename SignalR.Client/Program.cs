18353920db4267ec3f9ac9d36aa1076b465486c8
DESKTOP-5ND3FOT\Mohamed <mohamed.abozyd@gmail.com>
2018-03-08 23:05:05 +1300

    adding SignalR server and Client js, client console app as prototype

---
+++ SignalR.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.Client
+{
+    public  class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var hubConnection = new HubConnection("http://localhost:50960/");
+            IHubProxy testHub = hubConnection.CreateHubProxy("testHub");
+            testHub.On<string,string>("addNewMessageToPage", (name, message)=> { Console.WriteLine($"Message Recieved from:{name}, Message: {message}"); });
+            await hubConnection.Start();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                await testHub.Invoke("Send", "Mohamed", line);
+            }
+        }
+    }
+}
